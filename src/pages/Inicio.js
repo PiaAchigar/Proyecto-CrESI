0533cc1cb927ffb7b4c0ff3f9e30b43f76fc75d4
german <serigrafiatandil@gmail.com>
2021-04-15 19:17:18 -0300

    ...

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -12,12 +12,6 @@ const Inicio = () => {
         <Bubble height="10vw" top="0" left="20vw" naranja />
         <Bubble height="5vw" bottom="10%" right="0" verde />
       </BubbleContainer>
-
-      {/* 
-      <BubbleContainer content={<AprendeJugando />}>
-        <Bubble height="150px" top="-30vw" left="-30vw" gris />
-        <Bubble height="75px" bottom="20%" right="15vw" celeste />
-      </BubbleContainer> */}
     </div>
   );
 };
