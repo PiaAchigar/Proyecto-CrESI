6a2918bf1798c8d0cd503c41a8d09c1868cbc2bf
german <serigrafiatandil@gmail.com>
2021-04-17 12:10:48 -0300

    termine  secciones con responsive listo de la pagina inicio

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -4,13 +4,18 @@ import React from "react";
 import Bubble from "../components/BubbleContainer/Bubble";
 import BubbleContainer from "../components/BubbleContainer/BubbleContainer";
 import AprendeJugando from "../components/AprendeJugando/AprendeJugando";
+import QuienesSomos from "../components/QuienesSomos/QuienesSomos.js";
 
 const Inicio = () => {
   return (
     <div className="page-wrapper">
       <BubbleContainer content={<AprendeJugando />}>
-        <Bubble height="10vw" top="0" left="20vw" naranja />
-        <Bubble height="5vw" bottom="10%" right="0" verde />
+        <Bubble height="125px" top="-5%" left="-5%" gris />
+        <Bubble height="125px" top="30%" left="50%" celeste />
+      </BubbleContainer>
+      <BubbleContainer content={<QuienesSomos />}>
+        <Bubble height="175px" top="0" left="15%" naranja />
+        <Bubble height="125px" top="40%" right="10%" rosa />
       </BubbleContainer>
     </div>
   );
