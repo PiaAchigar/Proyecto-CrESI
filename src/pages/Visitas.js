9494272f5bf4d94ca5e113477069f754d8d743ca
Pia <achigarpia@gmail.com>
2021-05-06 18:03:05 -0300

    descktop css & Bubbles

--- src/pages/Visitas.js
+++ src/pages/Visitas.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ContenidoVisitas from "../components/ContenidoVisitas/ContenidoVisitas";
+import BubbleContainer from "../components/BubbleContainer/BubbleContainer";
+import Bubble from "../components/BubbleContainer/Bubble";
 
 const Visitas = () => {
   return (
     <div className="page-wrapper">
-      <ContenidoVisitas/>
+      <BubbleContainer content={<ContenidoVisitas/>}>
+        <Bubble height="160px" top="-1" left="-1%" amarillo />
+        <Bubble height="100px" top="40%" left="1%" violeta />
+        <Bubble height="160px" top="50%" right="5%" celeste />
+        <Bubble height="90px" top="80%" right="40%" rosa />
+        <Bubble height="90px" top="1%" right="5%" rosa />
+      </BubbleContainer>
     </div>
   );
 };
