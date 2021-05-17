05055dd2217a64c1ea75705282cd9be89ef7094a
gsaracca <gersaracca@gmail.com>
2021-05-17 12:07:09 -0300

    agrego form de contacto y libreria de ReCAPTCHA

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -2,11 +2,16 @@ import React from "react";
 
 //Components
 import Contactanos from "../components/Contactanos/Contactanos";
+import Bubble from "../components/BubbleContainer/Bubble";
+import BubbleContainer from "../components/BubbleContainer/BubbleContainer";
 
 const Contacto = () => {
   return (
     <div className="page-wrapper">
-      <Contactanos />
+      <BubbleContainer content={<Contactanos />} hidden>
+        <Bubble height="225px" top="0" left="0" celeste />
+        <Bubble height="55px" bottom="5%" right="25%" rosa />
+      </BubbleContainer>
     </div>
   );
 };
