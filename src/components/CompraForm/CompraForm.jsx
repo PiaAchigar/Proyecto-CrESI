351bdc87926ac02a66755612e82eeacb4d97242e
gsaracca <gersaracca@gmail.com>
2021-06-11 00:29:56 -0300

    carrito terminado

--- src/components/CompraForm/CompraForm.jsx
+++ src/components/CompraForm/CompraForm.jsx
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 //Componentes
 import CardTienda from "../CardTienda/CardTienda";
+//Context
+import { CartContext } from "../../context/cartContext";
 //CSS
 import "./CompraForm.scss";
 // ------ ARRAY DE PRODUCTOS ------ //
 import infoProductos from "../../assets/tienda-productos-info";
 
 const CompraForm = ({ cart, cartTotal }) => {
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phone, setPhone] = useState("");
+  const [mail, setMail] = useState("");
+  const histo = useHistory();
+
+  const { setCart } = useContext(CartContext);
+
+  const finalizarCompraHandler = (e) => {
+    e.preventDefault();
+
+    //Informacion del comprador
+    let usuarioInfo = {
+      name,
+      lastname,
+      phone,
+      mail,
+    };
+
+    //Elimino la propiedad img de cada prod
+    cart.forEach((e) => delete e.img);
+
+    //Informacion de compra ( datos comprador + detalle productos + total)
+    const compraInfo = {
+      buyer: usuarioInfo,
+      products: cart,
+      total: cartTotal,
+    };
+
+    alert(
+      "Develop : A continuacion se muestra la informacion final de la compra, sin la informacion del metodo de pago. Tambien se mostrara en consola. Luego redirige al home."
+    );
+    alert(JSON.stringify(compraInfo, null, 2));
+    console.log(compraInfo);
+
+    //Vacio el carrito de compra
+    setCart([]);
+    //Redirigo a la home
+    histo.push("/");
+  };
+
   return (
     <>
       <div className="compra-form">
@@ -14,74 +58,50 @@ const CompraForm = ({ cart, cartTotal }) => {
         <div className="line-gray-top"></div>
 
         <div className="content-wrapper">
-          <form className="form">
+          <form className="form" id="buy-form">
             <div className="input-group">
               <label htmlFor="nombre">Nombre</label>
               <input
                 type="text"
-                // onChange={(e) => setNombre(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
                 id="nombre"
                 placeholder="Ingresa tu nombre"
                 maxLength="50"
                 autoComplete="off"
-                required
               />
             </div>
             <div className="input-group">
               <label htmlFor="apellido">Apellido</label>
               <input
                 type="text"
+                onChange={(e) => setLastname(e.target.value)}
                 id="apellido"
                 placeholder="Ingresa tu apellido"
                 maxLength="50"
                 autoComplete="off"
-                required
               />
             </div>
             <div className="input-group">
               <label htmlFor="telefono">Teléfono</label>
               <input
                 type="tel"
+                onChange={(e) => setPhone(e.target.value)}
                 id="telefono"
                 placeholder="Ingresa tu teléfono"
                 maxLength="30"
                 autoComplete="off"
-                required
               />
             </div>
             <div className="input-group">
               <label htmlFor="email">E-mail</label>
               <input
                 type="email"
+                onChange={(e) => setMail(e.target.value)}
                 id="email"
                 placeholder="tuemail@mail.com"
-                autoComplete="off"
                 maxLength="50"
-                required
               />
             </div>
-            {/* <div className="input-group">
-          <label htmlFor="direccion">Direccion</label>
-          <input
-            type="text"
-            id="direccion"
-            placeholder="Ingresa tu dirección"
-            autoComplete="off"
-            maxLength="30"
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="ciudad">Localidad / Ciudad</label>
-          <input
-            type="text"
-            id="ciudad"
-            placeholder="Ingresa tu ciudad"
-            autoComplete="off"
-            maxLength="30"
-            required
-          />
-        </div> */}
           </form>
 
           <div className="line-gray"></div>
@@ -94,7 +114,12 @@ const CompraForm = ({ cart, cartTotal }) => {
             <div className="medios-de-pago">
               <h3>Medios de pago</h3>
             </div>
-            <button className="green-btn finalizar-compra-btn" type="submit">
+            <button
+              className="green-btn finalizar-compra-btn"
+              type="submit"
+              form="buy-form"
+              onClick={(e) => finalizarCompraHandler(e)}
+            >
               <p>Finalizar compra</p>
             </button>
           </div>
--- src/pages/FinalizarCompra.js
+++ src/pages/FinalizarCompra.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 //componentes
 import CompraForm from "../components/CompraForm/CompraForm";
 
@@ -7,6 +7,15 @@ import { CartContext } from "../context/cartContext";
 
 const FinalizarCompra = () => {
   const { cart, cartTotal } = useContext(CartContext);
+  //GO TOP
+  useEffect(() => {
+    setTimeout(() => {
+      window.scroll({
+        top: 0,
+        left: 0,
+      });
+    }, 250);
+  }, []);
 
   return (
     <div className="page-wrapper">
